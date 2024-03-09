b2cf94634b9cbc93a58578526d54a30ea6058957
Nikhil Gargatte <enikhilraj@yahoo.com>
2024-03-09 15:58:37 +0000

    Enable Spring Boot security and inject custom properties into the application.

--- src/main/java/edu/nick/mycoolapp/rest/FunRestController.java
+++ src/main/java/edu/nick/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,28 @@
 package edu.nick.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // inject property coach.name
+    @Value("${coach.name}")
+    // assign the value to the field coachName
+    private String coachName;
+
+    // inject property team.name
+    @Value("${team.name}")
+    // assign to the field teamName
+    private String teamName;
+
+    // expose new endpoint called "teaminfo" which will return coach name and team name
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
+
     // expose "/" that return "Hello World"
     @GetMapping("/")
     public String sayHello() {
